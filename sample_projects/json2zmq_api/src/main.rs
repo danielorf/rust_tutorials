3dd1d6dfeff91f8de86a2658a6c5a4049a486541
Daniel Orf <dorf@suse.com>
2019-06-23 21:22:12 -0700

    Added 'age' to show transformation before sending ot zmq server

--- sample_projects/json2zmq_api/src/main.rs
+++ sample_projects/json2zmq_api/src/main.rs
@@ -10,13 +10,14 @@ use nickel::{Nickel, JsonBody, HttpRouter};
 struct Person {
     first_name: String,
     last_name:  String,
+    age:        u8,
 }
 
 fn main() {
     let mut server = Nickel::new();
 
     // try it with curl
-    // curl 'http://localhost:6767' -H 'Content-Type: application/json;charset=UTF-8'  --data-binary $'{ "first_name": "John","last_name": "Connor" }'
+    // curl 'http://localhost:6767' -H 'Content-Type: application/json;charset=UTF-8'  --data-binary $'{ "first_name": "John","last_name": "Connor", "age": 35 }'
     server.post("/", middleware! { |request, response|
         println!("Running niclkel and zmq...");
 
@@ -28,9 +29,13 @@ fn main() {
         let person = try_with!(response, {
             request.json_as::<Person>().map_err(|e| (StatusCode::BadRequest, e))
         });
-        println!("Sending first name from json post to zmq: {}", person.first_name);
-        requester.send(&person.first_name, 0).unwrap();
-        format!("Hello {} {}", person.first_name, person.last_name)
+        println!("Sending message from json post to zmq: {} {} {}", person.first_name, person.last_name, person.age,);
+        let age_x2 = person.age * 2;
+
+        // Send the doubled age to the zmq server
+        requester.send(&age_x2.to_string(), 0).unwrap();
+
+        format!("Hello {} {} {}", person.first_name, person.last_name, person.age)
     });
 
     server.listen("127.0.0.1:6767").unwrap();
